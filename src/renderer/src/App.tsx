7a356e90526e6682fd075cb61e1cd89a56a1b814
5h1ngy <sig.scarano@outlook.it>
2025-10-08 07:25:00 +0200

    refactor(style): ♻️ move AppContainer styling to separate file
    - move AppContainer styling to separate file across style to cut complexity
    - make follow-up work safer by isolating responsibilities

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,84 +1,27 @@
-<<<<<<< HEAD
-import React, { useEffect, createElement } from 'react';
-import { Navigate, Outlet, Route, Routes } from "react-router-dom";
-import { HashRouter } from 'react-router-dom';
-
-import { ThemeProvider } from '@renderer/styles/ThemeProvider';
-import withSlice, { Bind } from '@renderer/hocs/withSlice';
-import withPublicRoute from '@renderer/hocs/withPublicRoute';
-import withProtectedRoute from '@renderer/hocs/withProtectedRoute';
-import AuthLayout from '@renderer/components/layouts/AuthLayout';
-import MainLayout from '@renderer/components/layouts/MainLayout';
-import loginRoute from '@renderer/pages/Login';
-import registerRoute from '@renderer/pages/Register';
-import dashboardRoute from '@renderer/pages/Dashboard';
-import projectsRoute from '@renderer/pages/Projects';
-import tasksRoute from '@renderer/pages/TaskBoard';
-import notesRoute from '@renderer/pages/Notes';
-import statisticsRoute from '@renderer/pages/Statistics';
-import settingsRoute from '@renderer/pages/Settings';
-import notFoundRoute from '@renderer/pages/NotFound/NotFound.component';
-import { AppContainer } from '@renderer/App.style';
-
-const App: React.FC<Bind> = (props: Bind) => {
-  const publicAuthLayout = createElement(withPublicRoute(AuthLayout, { redirect: "/dashboard" }))
-  const privateAuthLayout = createElement(withProtectedRoute(MainLayout, { redirect: '/login', children: <Outlet /> }))
-=======
 import { useEffect } from 'react';
-import { Navigate, Route, RouterProvider, Routes, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { useDispatch } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
 
-import { RootDispatch } from '@renderer/store';
+import { Dispatch } from '@renderer/store';
 import { restoreUser } from '@renderer/store/authSlice/asyncThunks';
 import { ThemeProvider } from '@renderer/styles/ThemeProvider';
 import { routes } from '@renderer/pages/routes';
 import { AppContainer } from './App.style';
-import PublicRoute from './hocs/PublicRoute';
-import AuthLayout from './components/layouts/AuthLayout';
-import LoginPage, { route } from './pages/LoginPage';
 
 const App: React.FC = () => {
-  const dispatch = useDispatch<RootDispatch>();
->>>>>>> 4a512eb (refactor(store): ♻️ restructure the Redux store for project management)
+  const dispatch = useDispatch<Dispatch>();
 
   useEffect(() => {
-    props.actions.authActions.restoreUser()
-  }, []);
+    dispatch(restoreUser());
+  }, [dispatch]);
 
   return (
     <ThemeProvider>
       <AppContainer>
-        <HashRouter>
-<<<<<<< HEAD
-          <Routes>
-            <Route path="/" element={publicAuthLayout}>
-              <Route index element={<Navigate to="/login" replace />} />
-              <Route path="login" {...loginRoute} />
-              <Route path="register" {...registerRoute} />
-            </Route>
-            <Route path="/" element={privateAuthLayout}>
-              <Route index element={<Navigate to="/dashboard" replace />} />
-              <Route path="dashboard" {...dashboardRoute} />
-              <Route path="projects/:projectId" {...projectsRoute} />
-              <Route path="projects/:projectId/tasks" {...tasksRoute} />
-              <Route path="notes" {...notesRoute} />
-              <Route path="statistics" {...statisticsRoute} />
-              <Route path="settings" {...settingsRoute} />
-              <Route path="*" {...notFoundRoute} />
-=======
-          {/* <RouterProvider router={createBrowserRouter(routes)} /> */}
-          <Routes>
-            <Route path="/" element={<PublicRoute children={<AuthLayout />} />}>
-              <Route index element={<Navigate to="/login" replace />} />
-              <Route path="login" loader={route.loader} action={route.action} element={route.element} />
->>>>>>> 4a512eb (refactor(store): ♻️ restructure the Redux store for project management)
-            </Route>
-          </Routes>
-        </HashRouter>
+        <RouterProvider router={createBrowserRouter(routes)} />
       </AppContainer>
     </ThemeProvider>
   );
 };
 
-export default withSlice(App);
+export default App;
