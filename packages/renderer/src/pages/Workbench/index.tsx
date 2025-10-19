95bab33b057da82dac90ec944fd7a899846730bb
5h1ngy <sig.scarano@outlook.it>
2025-10-19 06:50:00 +0200

    fix(workbench): 🐛 fine-tune workbench grid behavior
    - fine-tune workbench grid behavior inside workbench to stop the regression
    - add guards and tests so the issue cannot slip back in

--- packages/renderer/src/pages/Workbench/index.tsx
+++ packages/renderer/src/pages/Workbench/index.tsx
@@ -1,30 +1,30 @@
 import { Col, Row, Space } from 'antd'
 
+import DemoUploadCard from '@renderer/components/DemoUploadCard'
 import QuestionnaireStepper from '@renderer/components/QuestionnaireStepper'
-import ScoreCard from '@renderer/components/ScoreCard'
 import SectionCompletionCard from '@renderer/components/SectionCompletionCard'
 import SchemaSummaryCard from '@renderer/components/SchemaSummaryCard'
-import DemoUploadCard from '@renderer/components/DemoUploadCard'
+import ScoreCard from '@renderer/components/ScoreCard'
 
 const WorkbenchPage = () => (
   <Space direction="vertical" size="large" style={{ width: '100%' }}>
-    <Row gutter={[16, 16]} align="stretch">
-      <Col xs={24} lg={6} style={{ display: 'flex', minWidth: 0 }}>
+    <Row gutter={[16, 16]} justify="center" align="stretch">
+      <Col xs={24} xl={6}>
         <DemoUploadCard />
       </Col>
-      <Col xs={24} lg={18} style={{ display: 'flex', minWidth: 0 }}>
+      <Col xs={24} xl={18}>
         <QuestionnaireStepper />
       </Col>
     </Row>
-    <Row gutter={[16, 16]}>
-      <Col xs={24} lg={8}>
-        <SchemaSummaryCard />
-      </Col>
-      <Col xs={24} lg={8}>
+    <Row gutter={[16, 16]} justify="center" align="stretch">
+      <Col xs={24} xl={12}>
         <SectionCompletionCard />
       </Col>
-      <Col xs={24} lg={8}>
-        <ScoreCard />
+      <Col xs={24} xl={12}>
+        <Space direction="vertical" size="large" style={{ width: '100%' }}>
+          <SchemaSummaryCard />
+          <ScoreCard />
+        </Space>
       </Col>
     </Row>
   </Space>
